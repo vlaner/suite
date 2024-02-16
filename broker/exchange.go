1d4b5a9779fd0c5326201f9f601e5fb8673f5620
vlaner <vlaner@bk.ru>
2024-02-17 00:26:43 +0300

    handle dead messages

--- broker/exchange.go
+++ broker/exchange.go
@@ -2,21 +2,24 @@ package broker
 
 import (
 	"sync"
+	"time"
 )
 
 type Topic string
 
 type Exchange struct {
 	consumers map[Topic]Consumer
+	deadMsgs  []Message
 	msgsCh    chan Message
 	wg        sync.WaitGroup
-	sync      sync.RWMutex
+	sync      sync.Mutex
 	quit      chan struct{}
 }
 
 func NewExchange() *Exchange {
 	return &Exchange{
 		consumers: map[Topic]Consumer{},
+		deadMsgs:  []Message{},
 		msgsCh:    make(chan Message, 100),
 		wg:        sync.WaitGroup{},
 		quit:      make(chan struct{}),
@@ -49,16 +52,40 @@ func (e *Exchange) Publish(topic Topic, payload Payload) {
 }
 
 func (e *Exchange) ProcessMessage(msg Message) {
-	e.sync.RLock()
-	defer e.sync.RUnlock()
+	e.sync.Lock()
+	defer e.sync.Unlock()
 
 	consumer, exists := e.consumers[msg.topic]
-	if exists {
-		consumer.Consume(msg.payload)
+	if !exists {
+		e.deadMsgs = append(e.deadMsgs, msg)
+		return
 	}
+
+	consumer.Consume(msg.payload)
 }
 
 func (e *Exchange) ListenForMessages() {
+	go func() {
+		ticker := time.NewTicker(200 * time.Millisecond)
+		defer ticker.Stop()
+
+		for {
+			select {
+			case <-e.quit:
+				return
+			case <-ticker.C:
+				for _, msg := range e.deadMsgs {
+					e.wg.Add(1)
+					go func(msg Message) {
+						defer e.wg.Done()
+						e.ProcessMessage(msg)
+					}(msg)
+				}
+				e.deadMsgs = []Message{}
+			}
+		}
+	}()
+
 	go func() {
 		for message := range e.msgsCh {
 			e.wg.Add(1)
