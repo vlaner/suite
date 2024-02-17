c897bbd18e5e21dcb7ba827ef27e7a90a621225c
vlaner <vlaner@bk.ru>
2024-02-18 00:30:42 +0300

    add protocol writer

---
+++ protocol/writer.go
@@ -0,0 +1,23 @@
+package protocol
+
+import "io"
+
+type Writer struct {
+	writer io.Writer
+}
+
+func NewProtoWriter(w io.Writer) *Writer {
+	return &Writer{writer: w}
+}
+
+func (w Writer) Write(v Value) error {
+	b, err := v.Marshal()
+	if err != nil {
+		return err
+	}
+	_, err = w.writer.Write(b)
+	if err != nil {
+		return err
+	}
+	return nil
+}
